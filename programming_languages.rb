61004b16e874efff090de86cb854a3108cba5e93
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-18 17:03:27 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, hash|
+    hash.each do |language, hash2|
+      if new_hash[language] == nil
+        new_hash[language] = {:type => hash2[:type], :style => [style]}
+      else 
+        new_hash[language][:style] << style
+      end 
+    end 
+  end 
+  new_hash
 end
