f1bb61444b95c1f7d9a45c8cf9904973d9bdef76
Matthew McCormack <mmccormack1182@gmail.com>
2018-10-01 18:07:54 -0400

    Tests Passed

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, info|
+    info.each do |name, type|
+      if new_hash.has_key?(name)
+        new_hash[name][:style].push(style)
+      else
+        new_hash[name] = type
+        new_hash[name][:style] = [style]
+      end
+    end
+  end
+  new_hash
 end
