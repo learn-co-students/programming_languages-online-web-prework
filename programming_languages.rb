4c9df901cfdf20646fd158e5be8ab7acb912e163
Jessica Gladstone <jcg321@comcast.net>
2018-10-09 21:59:25 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,9 @@
 def reformat_languages(languages)
-  # your code here
-end
+  hash = {}
+  languages.each do |type, data|
+     data.each do |lang, value|
+      hash[lang]=value
+      hash[lang][:style]=type
+      end
+     end
+    end
