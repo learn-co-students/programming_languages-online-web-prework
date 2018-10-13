e2b4ea7c16cfd70278fab5d3cb3b255fced89094
Brandon Teich <brandonteich123@gmail.com>
2018-10-13 00:06:57 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,18 +4,20 @@ def reformat_languages(languages)
   languages.each do |x, y|
   attributes.each do |attribute, val|
    if language == :javascript
-    new[language] = {
-            :type => att_value,
-            :style => [:oo, :functional]
-          }
-        else
-          new[language] = {
-             :type => value,
-             :style => [language_cat]
-          }
-        end
+   new[language] = 
+    {
+      :type => att_value,
+       :style => [:oo, :functional]
+        }
+       else
+        new[language] = 
+        {
+           :type => value,
+           :style => [language_cat
+         }      
+       end
       end
     end
-  end
-  return new_hash
+ end
+  mew
 end
