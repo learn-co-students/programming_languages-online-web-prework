e3c0edfac7369241a27446044d3cff89eecbeb39
Kenneth Young Castro <kenneth.young90@gmail.com>
2018-10-15 00:24:57 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,82 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |language_style, language_data|
+    language_data.each do |language, language_info|
+      if new_hash.has_key?(language)
+        new_hash[language][:style].push(language_style)
+      else 
+        new_hash[language] = {
+        :type => language_info[:type], 
+        :style => [language_style]
+        }
+      end 
+      
+    end 
+  end 
+  
+  return new_hash
 end
+
+# languages = {
+#   :oo => {
+#     :ruby => {
+#       :type => "interpreted"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     },
+#     :python => {
+#       :type => "interpreted"
+#     },
+#     :java => {
+#       :type => "compiled"
+#     }
+#   },
+#   :functional => {
+#     :clojure => {
+#       :type => "compiled"
+#     },
+#     :erlang => {
+#       :type => "compiled"
+#     },
+#     :scala => {
+#       :type => "compiled"
+#     },
+#     :javascript => {
+#       :type => "interpreted"
+#     }
+ 
+#   }
+# }
+
+# {
+#   :ruby => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :javascript => {
+#     :type => "interpreted",
+#     :style => [:oo, :functional]
+#   },
+#   :python => {
+#     :type => "interpreted",
+#     :style => [:oo]
+#   },
+#   :java => {
+#     :type => "compiled",
+#     :style => [:oo]
+#   },
+#   :clojure => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :erlang => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   },
+#   :scala => {
+#     :type => "compiled",
+#     :style => [:functional]
+#   }
+# }
