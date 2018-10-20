6c275fd572253cd79e9ea8ab81e327fbebf724df
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-20 15:05:14 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = Hash.new
+  # hash of languages as keys, each has a value of another hash with two keys, type and styles
+  # the value of type is a string, where the value of style is an array
+  languages.each do |styles, lang_types|
+    new_hash[lang_types] = {}
+    new_hash[lang_types][:style] = [styles]
+  end
+  binding.pry
 end
