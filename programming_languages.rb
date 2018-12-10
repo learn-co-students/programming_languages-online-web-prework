0c11fa333c2b09bc97caf750e79d3056d67fd0b7
Robert McCary <robert.mccary@yahoo.com>
2018-12-10 22:24:05 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,35 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  new_hash = {
+    :ruby =>{
+      :type => "interpreted",
+      :style => [:oo]
+    },
+    :javascript =>{
+      :type => "interpreted",
+      :style => [:oo, :functional]
+    },
+    :python => {
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :java => {
+    :type => "compiled",
+    :style => [:oo]
+  },
+  :clojure => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :erlang => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :scala => {
+    :type => "compiled",
+    :style => [:functional]
+  }
+}
+
+
 end
