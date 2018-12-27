5fe2295fef4edd69895e792016b97d7c17043384
Joan in DC <joanrigdon@gmail.com>
2018-12-27 07:25:04 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,18 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  languages.each do |style, language|
+    language.each do |name, type|
+      #need following line to avoid error: undefined method `[]=' for nil:NilClass ??
+      if new_hash[name] == NIL
+        new_hash[name] = type
+        new_hash[name][:style] = []
+      end
+    new_hash[name][:style]<< style
+    end
+  end
+  new_hash
+  #binding.pry
 end
