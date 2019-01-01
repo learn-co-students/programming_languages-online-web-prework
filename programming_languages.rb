b6ff396b5f852b0bcce86b185015eff2abc128b6
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-01 06:04:09 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,20 @@
 require "pry"
 def reformat_languages(languages)
-h= {}
-  languages.each do |k,v|
-    add_to_hash=k
-    h=v
-    hash=v.keys
-    h.each do |language, key|
-     h[language]={:style =>[ ]}
-#  h[language][:style]     <<add_to_hash
+  h= {}
+  languages.each do |k,lang_hash|
+    lang_hash.each do |lang,style|
+      if !h[lang]
+        h[lang]=style
+      end
+      if   !h[lang][:style]
+        h[lang][:style]= [k]
+      else
+      h[lang][:style] << k
 
-end
-end
-h
+
+      end
+    end
+    end
+
+    h
 end
