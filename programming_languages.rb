e1407f7234ceb8925802f757c2bcff200f7437b7
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-08 04:19:49 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,53 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  output = {}
+  languages.each do |style, languages_hsh|
+    languages_hsh.each do |language, type_hsh|
+      if output.has_key?(language)
+        output[language][:style] << style
+      else
+        output[language] = {}
+        output[language][:style] = [style]
+        type_hsh.each do |type_key, type|
+          output[language][:type] = type
+        end
+      end
+    end
+  end
+  output
 end
+
+# let(:languages_by_style) {
+#   {
+#     :oo => {
+#       :ruby => {
+#         :type => "interpreted"
+#       },
+#       :javascript => {
+#         :type => "interpreted"
+#       },
+#       :python => {
+#         :type => "interpreted"
+#       },
+#       :java => {
+#         :type => "compiled"
+#       }
+#     },
+#     :functional => {
+#       :clojure => {
+#         :type => "compiled"
+#       },
+#       :erlang => {
+#         :type => "compiled"
+#       },
+#       :scala => {
+#         :type => "compiled"
+#       },
+#       :javascript => {
+#         :type => "interpreted"
+#       }
+#
+#     }
+#   }
+# }
