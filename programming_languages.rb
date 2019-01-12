ef2adb9b56c42b611aef1c3a38e7f71f5a6661d3
Max Brooks <maxbrooks114@gmail.com>
2019-01-12 19:04:13 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,29 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash={}
+  new_hash[:ruby]= languages[:oo][:ruby]
+  new_hash[:ruby][:style]= [languages.keys.first]
+ 
+  new_hash[:javascript]= languages[:oo][:javascript]
+  new_hash[:javascript][:style]=languages.keys
+  
+  new_hash[:python]= languages[:oo][:python]
+  new_hash[:python][:style]=[languages.keys.first]
+  
+  new_hash[:java]= languages[:oo][:java]
+  new_hash[:java][:style]=[languages.keys.first]
+  
+  new_hash[:clojure]= languages[:functional][:clojure]
+  new_hash[:clojure][:style]=[languages.keys.last]
+  
+  new_hash[:erlang]= languages[:functional][:erlang]
+  new_hash[:erlang][:style]=[languages.keys.last]
+  
+  new_hash[:scala]= languages[:functional][:scala]
+  new_hash[:scala][:style]=[languages.keys.last]
+ 
+  new_hash
+
+    
 end
