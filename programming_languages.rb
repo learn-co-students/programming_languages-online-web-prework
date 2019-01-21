73463a2cbd641a84c22d0a02cfc0a350bb6cdc09
Vinh Do <vinhdo07@gmail.com>
2019-01-21 21:56:38 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,41 +1,19 @@
 require 'pry'
 
-# def reformat_languages(languages)
-# new_hash = (languages)
-#   new_hash.each_value do |prog_lang|
-#     prog_lang.map do |key, value|
-#       if key == :javascript
-#         value[:style] = [:oo, :functional]
-#       elsif key == :java
-#         value[:style] = [:oo]
-#       elsif (value[:type] == "interpreted" && key != :javascript)
-#         value[:style] = [:oo]
-#       elsif (value[:type] == "compiled" && key != :java)
-#         value[:style] = [:functional]
-#       end
-#     end
-#   end
-#   new_hash
-#   binding.pry
-# end
-
-
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style_key, lang_val|
     lang_val.each do |lang_key, lang_attr|
       new_hash[lang_key] = lang_attr
+      new_hash[lang_key][:style] = []
     end
-
-    new_hash.each do |hash_key, hash_val|
-      if style_key == :oo && lang_key == hash_key
-        new_hash[:style] = [style_key]
-        binding.pry
-      elsif style_key == :functional && lang_key == hash_key
-        new_hash[:style] = [style_key]
+    new_hash.map do |hash_key, hash_value|
+      if hash_key == :javascript
+        hash_value[:style] = [:oo, :functional]
+      elsif (lang_val.keys.include?(hash_key)) && !(hash_value[:style].include?(style_key))
+        hash_value[:style] << style_key
       end
+    end
   end
-
   new_hash
-  binding.pry
 end
