d238870cc82f3e9713ad23ee195e5604440264ec
Christoph Gruener <christophgruener@me.com>
2019-02-07 19:20:55 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,25 +9,16 @@ def reformat_languages(languages)
         new_language_hash[language][key] = types
       end
       new_language_hash[language][:style] = []
-      new_language_hash[language][:style] << styles
     end
   end
   
   
-  
-  
-  
+  languages.each do |styles, language_data|
+    language_data.each do |language_name, type|
+      new_language_hash[language_name][:style] << styles
+    end
+  end
 
-      
- 
-          
 new_language_hash  
 end
 
-
-##  new_language_hash.each do |language_name, data|
-##    languages.each do |styles, language_data|
-##      language_data.each do |key, value|
-##        value.each do |type, values|
-##          if key == language_name
-##            new_language_hash[language_name] << value
