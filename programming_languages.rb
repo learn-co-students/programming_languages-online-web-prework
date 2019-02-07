77880122db4c1d1a204ddc9246f2812fc135d61c
Lu Daley <daleyluis@gmail.com>
2019-02-07 21:03:29 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,74 @@
+require 'pry'
+=begin
 def reformat_languages(languages)
-  # your code here
+hash = {}
+#binding.pry
+  languages.each do |k,v|
+    v.map do |lang,type|
+      type.each do |attribute,handling|
+        binding.pry
+       hash[lang] = {}
+    #  hash[lang][:]
+      #{type,style}[]
+
+#binding.pry
+    end
+   end
+  end
+  hash
+end
+=end
+#reformat_languages reformats ruby properly
+#     Failure/Error: ruby.each { |k,v| expect(result[:ruby][k]).to eq(v) }
+
+=begin
+It should match the following format:
+{
+  :ruby => {
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :javascript => {
+    :type => "interpreted",
+    :style => [:oo, :functional]
+  },
+  :python => {
+    :type => "interpreted",
+    :style => [:oo]
+  },
+  :java => {
+    :type => "compiled",
+    :style => [:oo]
+  },
+  :clojure => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :erlang => {
+    :type => "compiled",
+    :style => [:functional]
+  },
+  :scala => {
+    :type => "compiled",
+    :style => [:functional]
+  }
+}
+
+
+=end
+
+def reformat_languages(languages)
+  new_hash = {}
+  languages.each do |style, v|
+    v.each do |language, data|
+      data.each do |type, info|
+        if new_hash[language] == nil
+          new_hash[language]= { type=> info, :style => [style]}
+        else
+          new_hash[language][:style] << style
+        end
+      end
+    end
+  end
+    return new_hash
 end
