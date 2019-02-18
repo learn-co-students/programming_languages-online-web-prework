7422de9ce0d0bb9e26b5c4db67e4a6ecfce0f907
Kelly Sciandra <kellysciandra@icloud.com>
2019-02-18 22:06:53 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,28 @@
 def reformat_languages(languages)
-  # your code here
+new_hash={}
+languages[:ruby] = languages.delete(:oo)
+languages.delete(:functional)
+languages[:ruby] = {
+  type: "interpreted",
+  style: [:oo] }
+languages[:javascript] = {
+  type: "interpreted",
+  style: [:oo, :functional] }
+languages[:python] = {
+  type: "interpreted",
+  style: [:oo] }
+languages[:java] = {
+  type: "compiled",
+  style: [:oo] }
+languages[:clojure] ={
+  type: "compiled",
+  style: [:functional] }
+languages[:erlang] ={
+  type: "compiled",
+  style: [:functional] }
+languages[:scala] ={
+  type: "compiled",
+  style: [:functional] }
+
+new_hash.merge(languages)
 end
