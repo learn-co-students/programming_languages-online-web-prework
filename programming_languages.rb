bb8fbdcc7d4bf79d132680d2d8440d62e95cb9c6
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-18 19:59:43 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -17,21 +17,20 @@ def reformat_languages(languages)
 
   languages.each do |style, language_hash|
     language_hash.each do |language, attributes|
-      new_hash[language] = {style: []}
+      new_hash[language] = {
+        type: attributes.values.join,
+        style: []
+      }
     end
   end
   
   languages.each do |style, language_hash|
     language_hash.each do |language, attributes|
-
-      new_hash = {
-      attributes, 
-      binding.pry
       new_hash[language][:style] << style
-      }
-
     end
   end
+  
+  new_hash
 
   # languages.each do |style, language_hash|
   #   # binding.pry
