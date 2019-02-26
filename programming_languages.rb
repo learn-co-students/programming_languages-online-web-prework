95fd02e2cd3af82c27df05b510c6d01f276ecd26
Ryan Sise <kerneltux0@gmail.com>
2019-02-26 21:06:15 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,68 @@
 def reformat_languages(languages)
-  # your code here
+	new_hash = {}
+	languages.each do |category,langs|
+		langs.each do |name,aspect|
+			aspect.each do |typeKey,typeVal|
+				new_hash[name] = {
+					:type => typeVal,
+					:style => []
+				}
+				new_hash[name][:style] << category
+			end
+		end
+	end
+	new_hash[:javascript][:style] << :oo
+	new_hash
 end
+
+def lang
+  {
+    :oo => {
+      :ruby => {
+        :type => "interpreted"
+      },
+      :javascript => {
+        :type => "interpreted"
+      },
+      :python => {
+        :type => "interpreted"
+      },
+      :java => {
+        :type => "compiled"
+      }
+    },
+    :functional => {
+      :clojure => {
+        :type => "compiled"
+      },
+      :erlang => {
+        :type => "compiled"
+      },
+      :scala => {
+        :type => "compiled"
+      },
+      :javascript => {
+        :type => "interpreted"
+      }
+   
+    }
+  }
+end
+
+def practice
+	new_hash = {}
+	lang.each do |category,langs|
+		langs.each do |name,aspect|
+			aspect.each do |typeKey,typeVal|
+				new_hash[name] = {
+					:type => typeVal,
+					:style => []
+				}
+				new_hash[name][:style] << category
+			end
+		end
+	end
+	puts new_hash
+end
+
+practice
