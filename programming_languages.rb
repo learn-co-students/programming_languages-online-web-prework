45e1c4aef7386d962daadb9387ddd327993d4cae
Natasha Chernis <natashachernis@gmail.com>
2019-03-08 03:51:21 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_language_hash = {}
+
+  languages.each {|style, languages|
+    languages.each {|language, attribute|
+
+      if new_language_hash[language].class == Hash
+        new_language_hash[language][:style] << style
+      else
+        new_language_hash[language] = {}
+        new_language_hash[language] = attribute
+        new_language_hash[language][:style] = [style]
+      end
+
+    }
+  }
+  
+  new_language_hash
 end
