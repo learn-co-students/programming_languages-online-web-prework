e318107409dbe8923177741aacd1513f2881001e
Ethan <ethansagin@gmail.com>
2019-03-22 19:09:11 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, lang|
+    lang.each do |lang_k, lang_v|
+      new_hash[lang_k] ||= lang_v
+      new_hash[lang_k][:style] ||= []
+      new_hash[lang_k][:style] << style
+    end
+  end
+  new_hash
 end
