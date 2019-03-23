c71a24726988ff85d44a73b294f0508c098be6f9
Charlotte Goldthwaite <chargold201@gmail.com>
2019-03-23 17:07:49 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,11 @@
-require 'pry'
-
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, langs|
     langs.each do |name, attributes|
-      new_hash[name] = attributes
-      new_hash[name][:style] = []
+      new_hash[name] ||= attributes
+      new_hash[name][:style] ||= []
       new_hash[name][:style] << style
-      binding.pry
     end
-
   end
   new_hash
 end
