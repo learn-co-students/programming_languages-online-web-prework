a2bbede3c0fa4b44ace0428ab33f90a9eda25862
Aaron Parkening <aaron.parkening@gmail.com>
2019-04-08 15:45:13 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,61 @@
+require 'pry'
+
+=begin
+# Set languages hash
+languages ={
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+  }
+}
+=end
+
+# Build new programming language hash with languages as keys
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  
+  # Iterate through existing hash by style
+  languages.each do |style, lang_stats|
+    lang_stats.each do |name, type|
+        
+      # If language exists, add to style array
+      if new_hash[name]
+        new_hash[name][:style] << style
+        
+      # Else create new hash entry
+      else 
+        new_hash[name] = {
+          :type => type[:type],
+          :style => [style]
+        }
+      end
+    end
+  end
+  
+  new_hash 
+
 end
