e9160b6b3f783772fda9616d829fce30775f7f95
Caleb Einarr <earthctzn1@gmail.com>
2019-04-12 06:12:58 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,12 @@
-def reformat_languages(languages)
+def reformat_languages(l)
   # your code here
+  hash = {}
+  l.each do |style, lang|
+    lang.each do |name, attributes|
+      hash[name] ||= attributes
+      hash[name][:style] ||= []
+      hash[name][:style] << style
+    end
+  end
+  new_hash
 end
