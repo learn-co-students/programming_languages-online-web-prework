27ba471b6b6f71b76834e7ab065becf860596191
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-14 15:43:28 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,30 @@
+require 'pry'
 def reformat_languages(languages)
-  # your code here
-end
+  new_hash = {}
+  keys = []
+  languages.each do |key, value|
+    value.each do |language_names, info|
+      new_hash[language_names] = info
+      new_hash[language_names][:style] = []
+    end   
+    new_hash.each do |language, info|
+      keys = []
+      languages.each do |key, value|
+        #binding.pry
+        if value.include?(language)
+            keys << key
+            new_hash[language][:style] = keys.uniq
+            #binding.pry
+        end
+        #binding.pry
+      end
+      #binding.pry
+    end
+    #binding.pry
+  end
+  #binding.pry
+  new_hash
+ end          
+    
+      
+  
