e3cfc53f9a59edc5dd6da6a160540f39aa042724
Boon Sim Oh <boonsim.oh15@gmail.com>
2019-04-28 23:32:47 +0800

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,21 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash={}
+  languages.each do |type,data|
+    data.each do |lang,value|
+      new_hash[lang]={type:value.values[0],style:[type]}
+    end
+  end
+  languages.each do |type,data|
+    new_hash.each do |lang,value|
+      new_hash[lang][:style].push(type) if data.include?(lang)
+      #making sure that languages with two styles ":oo" or ":functional"
+      #have those in the style array
+      new_hash[lang][:style]=new_hash[lang][:style].uniq
+      #the line above this one adds duplicates of the style so we
+      #need to make it unique
+    end
+  end
+  new_hash
 end
