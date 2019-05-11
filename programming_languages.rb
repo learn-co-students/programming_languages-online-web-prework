286b2c55346f0598ec76c78d7038b9cb3227e88f
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-05-11 21:27:18 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,24 +4,21 @@ def reformat_languages(languages)
   new = {}
 
   languages.each do |key, values|
-    #puts values
+    
      values.each do |p_key, type|
-     #puts p_key
+
       type.each do |c_key, result|
-     #puts result
+   
         new[p_key] ||= {}
         new[p_key][:type] ||= result
         new[p_key][:style] ||= []    
         new[p_key][:style] << key  
-    #       end
-
-    #     end            
+        
       end
     
      end
      new
   end
 
- 
     new
 end
