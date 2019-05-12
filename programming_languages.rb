7a3fb9b72f2e5712a522ea03f691f7065a890498
Kristin Kraemer <kristinhannahk@gmail.com>
2019-05-12 17:33:52 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,79 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |styles, lang|
+    lang.each do |name, type|
+       type.each do |key, value|
+        # unless new_hash.include?(name) 
+         if new_hash.include?(name)
+           new_hash[name][:style] << styles
+         else
+            new_hash[name] = {key => value}
+            new_hash[name][:style] = [styles]
+       #  if new_hash.include?(name)
+         #  new_hash[name][:style] << styles
+         #  [[styles]] ||= new_hash[name][:style][[:oo, :functional]]
+
+        end
+       end 
+       end 
+     end
+  return new_hash
 end
+
+#def reformat_languages(languages)
+#  new_hash = {}
+ # languages.each do |styles, lang|
+ #   lang.each do |name, type|
+ #      type.each do |key, value|
+  #       unless new_hash.include?(name) 
+  #         new_hash[name] = {key => value, :style => [styles]}
+   #        unless new_hash[name][:style].include?(styles)
+    #          new_hash[name][:style] << styles
+     #    end 
+      #   end
+#       end 
+ #    end
+  #  end 
+#  return new_hash
+# end
+
+#h[:key] << "bar"
+
+# styles = :oo, functional 
+# lang = :ruby => {type => interpreted"}, etc.. 
+# name =: ruby, 
+#type = :type => "interpreted"
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+ 
+  }
+}
