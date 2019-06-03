d2feb30b10ed76139f05dc1792f03ac96f691a04
Huy Do <huyddo40@gmail.com>
2019-06-03 02:59:33 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,28 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  hash = {}
+  # styles =[]
+  languages.each do |key,value|
+    # styles << key
+    # puts "#{styles}"
+    value.each do |k,v|
+      v.each do |t,val|
+        if hash[k].nil?
+          hash[k] = {}
+        end
+        #create empty array if :style is not exist
+        hash[k][:style] ||= []
+        hash[k][:style] << key
+        if hash[k][t].nil?
+          hash[k][t] = val
+        end
+      end
+      # if hash[k]
+      #   hash[:type] = v
+      #   hash[k][:style] = styles
+      # end
+    end
+  end
+  hash
+  # binding.pry
 end
