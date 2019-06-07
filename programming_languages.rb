ab89739a7a2a5425a3a37870fb906db1469b55db
Alex Nahimana <alex.nahimana1@gmail.com>
2019-06-07 15:32:05 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,15 +1,27 @@
-
 def reformat_languages(languages)
   # Declare some variables
   new_hash = {}
-
-  # Iterate over the languages
-  languages.each do |style, style_attr|
-    style_attr.each do |lang, lang_attr|
-      new_hash[lang] = lang_attr
-      new_hash[lang][:style] = [style]
+  type_cat = []
+  # Iterate over language
+  # Update new_hash
+  languages.each do |style, style_info|
+    type_cat << style = style_info.keys
+    style_info.each do |lang, detials|
+      new_hash[lang] = detials
+      new_hash[lang][:style] = []
     end
   end
-  new_hash[:javascript][:style].insert(-1, :oo)
-  new_hash
+
+  # Operate on newly created hash
+  # Add style of each lang
+  new_hash.each do |lang, data|
+
+    languages.keys.each_with_index do |style, index|
+      if type_cat[index].include?(lang)
+        # Update style array of each lang
+        new_hash[lang][:style] << style
+      end
+
+    end
+  end
 end
